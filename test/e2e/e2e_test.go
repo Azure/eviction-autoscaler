a6885cab3b0d20dcbb84b3d4d20d6570ad95a648
sushmak295 <sushmak295@gmail.com>
2025-03-20 14:53:47 -0700

    e2e test: test CRUD behavior for an nginx deployment

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -19,6 +19,8 @@ package e2e
 import (
 	"context"
 	"fmt"
+
+	"log"
 	"os/exec"
 	"path/filepath"
 	"time"
@@ -28,6 +30,8 @@ import (
 	policy "k8s.io/api/policy/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -115,6 +119,11 @@ var _ = Describe("controller", Ordered, func() {
 			clientset, err := kubernetes.NewForConfig(config)
 			Expect(err).NotTo(HaveOccurred())
 
+			dynamicClient, err := dynamic.NewForConfig(config)
+			if err != nil {
+				log.Fatalf("Error creating dynamic Kubernetes client: %v", err)
+			}
+
 			By("validating that the controller-manager pod is running as expected")
 			var nodeName string
 			verifyOneRunningPod := func() error {
@@ -133,6 +142,67 @@ var _ = Describe("controller", Ordered, func() {
 				return nil
 			}
 			EventuallyWithOffset(1, verifyOneRunningPod, time.Minute, time.Second).Should(Succeed())
+
+			By("validating that the nginx pod is running as expected")
+			//var nodeName string
+			verifyOneRunningNginxPod := func() error {
+				pods, err := clientset.CoreV1().Pods("ingress-nginx").List(ctx, v1.ListOptions{
+					LabelSelector: "app.kubernetes.io/component=controller", Limit: 1,
+				})
+				ExpectWithOffset(1, err).NotTo(HaveOccurred())
+				if len(pods.Items) != 1 {
+					return fmt.Errorf("got %d nginx pods", len(pods.Items))
+				}
+				if pods.Items[0].Status.Phase != "Running" {
+					return fmt.Errorf("nginx pod in %s status", pods.Items[0].Status.Phase)
+				}
+				fmt.Printf("nginx pod %s running on %s\n", pods.Items[0].Name, pods.Items[0].Spec.NodeName)
+				//nodeName = pods.Items[0].Spec.NodeName
+				return nil
+			}
+			EventuallyWithOffset(1, verifyOneRunningNginxPod, time.Minute, time.Second).Should(Succeed())
+			By("Verify PDB and PDBWatcher exist")
+			verifyPdbExists := func() error {
+				pdbList, err := clientset.PolicyV1().PodDisruptionBudgets("ingress-nginx").List(ctx,
+					v1.ListOptions{Limit: 1})
+				Expect(err).NotTo(HaveOccurred())
+				fmt.Printf("found %d pdbs in namespace ingress-nginx\n", len(pdbList.Items))
+				for _, pdb := range pdbList.Items {
+					fmt.Printf("found pdb name: %s \n", pdb.Name)
+					if pdb.Name != "ingress-nginx-controller" {
+						return fmt.Errorf("nginx pdb is not present on cluster")
+					}
+					if pdb.Spec.MinAvailable != nil {
+						if val := pdb.Spec.MinAvailable.IntValue(); val != 1 {
+							return fmt.Errorf("PDB '%s' has MinAvailable set to %d (not 1)\n", pdb.Name, val)
+						}
+						fmt.Printf("PDB '%s' has MinAvailable set to 1\n", pdb.Name)
+					}
+				}
+				return nil
+			}
+			verifyPdbWatcherExists := func() error {
+				gvr := schema.GroupVersionResource{
+					Group:    "apps.mydomain.com", // API group for your custom resource
+					Version:  "v1",                // API version for the custom resource
+					Resource: "pdbwatchers",       // Resource name (plural form of your custom resource)
+				}
+				pdbWatcherList, err := dynamicClient.Resource(gvr).Namespace("ingress-nginx").List(ctx,
+					v1.ListOptions{Limit: 1})
+				Expect(err).NotTo(HaveOccurred())
+				fmt.Printf("found %d pdbwatchers in namespace ingress-nginx \n", len(pdbWatcherList.Items))
+				for _, resource := range pdbWatcherList.Items {
+					fmt.Printf("found pdbwatcher name: %s \n", resource.GetName())
+					if resource.GetName() != "ingress-nginx-controller" {
+						return fmt.Errorf("nginx pdbwatcher is not present on cluster")
+					}
+					fmt.Printf("custom resource found: %s", resource.GetName())
+				}
+				return nil
+			}
+			EventuallyWithOffset(1, verifyPdbExists, time.Minute, time.Second).Should(Succeed())
+			EventuallyWithOffset(1, verifyPdbWatcherExists, time.Minute, time.Second).Should(Succeed())
+
 			By("By Cordoning " + nodeName)
 			// Cordon and drain the node that the controller-manager pod is running on
 			node, err := clientset.CoreV1().Nodes().Get(ctx, nodeName, v1.GetOptions{})
@@ -182,6 +252,81 @@ var _ = Describe("controller", Ordered, func() {
 			EventuallyWithOffset(1, verifyDeploymentReplicas, 2*time.Minute, time.Second).Should(Succeed())
 			By("Verifying we only have one pod left")
 			EventuallyWithOffset(1, verifyOneRunningPod, time.Minute, time.Second).Should(Succeed())
+
+			By("Manually scaling Deployment replicas up")
+			scaleNginxReplicas := func() error {
+				cmd := exec.Command("kubectl", "scale", "deployment", "ingress-nginx-controller", "--replicas", "2", "-n", "ingress-nginx")
+
+				// Run the command and capture the output
+				output, err := cmd.CombinedOutput()
+				if err != nil {
+					log.Printf("Error scaling deployment: %v\n", err)
+					return err
+				}
+
+				// Log the output from the command (success message)
+				log.Printf("Scaled deployment %s to %d replicas: %s\n", "ingress-nginx-controller", 2, output)
+				return nil
+			}
+			EventuallyWithOffset(1, scaleNginxReplicas, time.Minute, time.Second).Should(Succeed())
+
+			By("Verify PDB MinAvailable is 2")
+			verifyMinAvailableUpdated := func() error {
+				pdbList, err := clientset.PolicyV1().PodDisruptionBudgets("ingress-nginx").List(ctx,
+					v1.ListOptions{Limit: 1})
+				Expect(err).NotTo(HaveOccurred())
+				fmt.Printf("found %d pdbs in namespace ingress-nginx\n", len(pdbList.Items))
+				for _, pdb := range pdbList.Items {
+					fmt.Printf("found pdb name: %s \n", pdb.Name)
+					if pdb.Spec.MinAvailable != nil {
+						if val := pdb.Spec.MinAvailable.IntValue(); val != 2 {
+							return fmt.Errorf("PDB '%s' has MinAvailable set to %d (not 2)\n", pdb.Name, val)
+						}
+						fmt.Printf("PDB '%s' has MinAvailable set to 2\n", pdb.Name)
+					}
+				}
+				return nil
+			}
+			EventuallyWithOffset(1, verifyMinAvailableUpdated, time.Minute, time.Second).Should(Succeed())
+
+			By("Delete Deployment and verify PDB gets deleted")
+			deleteNginxDeployment := func() error {
+				err = clientset.AppsV1().Deployments("ingress-nginx").Delete(context.TODO(), "ingress-nginx-controller", v1.DeleteOptions{})
+				if err != nil {
+					return fmt.Errorf("Error deleting Deployment %s in namespace %s: %v", "ingress-nginx-controller", "ingress-nginx", err)
+				}
+
+				fmt.Printf("Deployment '%s' deleted successfully in namespace '%s'.\n", "ingress-nginx-controller", "ingress-nginx")
+				return nil
+			}
+			EventuallyWithOffset(1, deleteNginxDeployment, time.Minute, time.Second).Should(Succeed())
+			verifyPdbNotExists := func() error {
+				pdbList, err := clientset.PolicyV1().PodDisruptionBudgets("ingress-nginx").List(ctx,
+					v1.ListOptions{Limit: 1})
+				Expect(err).NotTo(HaveOccurred())
+				fmt.Printf("found %d pdbs in namespace ingress-nginx\n", len(pdbList.Items))
+				if len(pdbList.Items) != 0 {
+					return fmt.Errorf("nginx pdb is still present on cluster")
+				}
+				return nil
+			}
+			verifyPdbWatcherNotExists := func() error {
+				gvr := schema.GroupVersionResource{
+					Group:    "apps.mydomain.com", // API group for your custom resource
+					Version:  "v1",                // API version for the custom resource
+					Resource: "pdbwatchers",       // Resource name (plural form of your custom resource)
+				}
+				pdbWatcherList, err := dynamicClient.Resource(gvr).Namespace("ingress-nginx").List(ctx,
+					v1.ListOptions{Limit: 1})
+				Expect(err).NotTo(HaveOccurred())
+				fmt.Printf("found %d pdbwatchers in namespace ingress-nginx \n", len(pdbWatcherList.Items))
+				if len(pdbWatcherList.Items) != 0 {
+					return fmt.Errorf("nginx pdbwatcher is still present on cluster")
+				}
+				return nil
+			}
+			EventuallyWithOffset(1, verifyPdbNotExists, time.Minute, time.Second).Should(Succeed())
+			EventuallyWithOffset(1, verifyPdbWatcherNotExists, time.Minute, time.Second).Should(Succeed())
 		})
 	})
 })

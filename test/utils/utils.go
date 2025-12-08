41972ee165a3b638df309d35c3fffe8879988eb7
Paul Miller <pmiller@microsoft.com>
2025-12-08 22:43:34 +0000

    missed some cert manager/gen stuff

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -29,9 +29,6 @@ const (
 	prometheusOperatorVersion = "v0.72.0"
 	prometheusOperatorURL     = "https://github.com/prometheus-operator/prometheus-operator/" +
 		"releases/download/%s/bundle.yaml"
-
-	certmanagerVersion = "v1.14.4"
-	certmanagerURLTmpl = "https://github.com/jetstack/cert-manager/releases/download/%s/cert-manager.yaml"
 )
 
 // InstallPrometheusOperator installs the prometheus Operator to be used to export the enabled metrics.

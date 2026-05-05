8ef0d4c117aea66e44eb5ca9bfa9feae5523f85d
sushmak295 <sushmak295@gmail.com>
2026-05-05 14:41:04 -0700

    fix e2e

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -98,10 +98,10 @@ var _ = Describe("controller", Ordered, func() {
 		//utils.UninstallPrometheusOperator()
 
 		if cleanEnv {
-			//
-			//By("removing kind cluster")
-			//cmd := exec.Command("kind", "delete", "cluster", "-n", kindClusterName)
-			//_, _ = utils.Run(cmd)
+
+			By("removing kind cluster")
+			cmd := exec.Command("kind", "delete", "cluster", "-n", kindClusterName)
+			_, _ = utils.Run(cmd)
 		}
 	})
 
